641989e869ac4c9840a0d698c0e03650a4d3b48e
dhwdeca <109137275+dhwdeca@users.noreply.github.com>
2022-07-13 17:12:41 +0200

    Update streamlit_app.py
    fixing insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-my_cur.execute("INSERT INTO fruit_load_list VALUES " + add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
+my_cur.execute("INSERT INTO fruit_load_list VALUES ('" + add_my_fruit + "')")
