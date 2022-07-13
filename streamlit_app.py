9146116af5d201a886eb909aa5a66566402b9213
dhwdeca <109137275+dhwdeca@users.noreply.github.com>
2022-07-13 14:10:04 +0200

    Update streamlit_app.py
    deleted raw line

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 # normalize the json data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show the normalized data in a dataframe
